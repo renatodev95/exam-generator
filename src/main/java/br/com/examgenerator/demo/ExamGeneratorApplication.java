c2329fc0ffe9070f35367373a1bf4b5ce27b5261
Renato Pereira Santos <77284595+renatodev95@users.noreply.github.com>
2022-01-05 17:22:58 -0300

    criacao do projeto, database config, criando os primeiros models

---
+++ src/main/java/br/com/examgenerator/demo/ExamGeneratorApplication.java
@@ -0,0 +1,11 @@
+package br.com.examgenerator.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamGeneratorApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ExamGeneratorApplication.class, args);
+	}
+}
---
+++ src/main/java/br/com/examgenerator/demo/persistence/model/AbstractEntity.java
@@ -0,0 +1,40 @@
+package br.com.examgenerator.demo.persistence.model;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import java.io.Serializable;
+import java.util.Objects;
+
+import static javax.persistence.GenerationType.AUTO;
+
+@MappedSuperclass
+public class AbstractEntity implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = AUTO)
+    protected Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        AbstractEntity that = (AbstractEntity) o;
+
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+}
